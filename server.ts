82bcd03c4ef11c880324b60cf5fb5cc1e420d127
Kishore-Dev <kishoreprakash45@gmail.com>
2026-07-31 23:57:08 +0530

    Import GoogleGenAI from @google/genai

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import { createServer as createViteServer } from "vite";
-
+import { GoogleGenAI } from "@google/genai";
 
 
 dotenv.config();
